fe1e73fe8a308b96d1e3ad2e56e36879ace6061d
Urban <dzindzinovic@yahoo.com>
2020-08-25 22:55:58 +0200

    Version 0.1.0.0

---
+++ Hardware.Info/Components/MemoryStatus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hardware.Info
+{
+    public class MemoryStatus
+    {
+        public ulong TotalPhysical { get; internal set; }
+        public ulong AvailablePhysical { get; internal set; }
+        public ulong TotalPageFile { get; internal set; }
+        public ulong AvailablePageFile { get; internal set; }
+        public ulong TotalVirtual { get; internal set; }
+        public ulong AvailableVirtual { get; internal set; }
+        public ulong AvailableExtendedVirtual { get; internal set; }
+
+        public override string ToString()
+        {
+            return
+                "TotalPhysical: " + TotalPhysical + Environment.NewLine +
+                "AvailablePhysical: " + AvailablePhysical + Environment.NewLine +
+                "TotalPageFile: " + TotalPageFile + Environment.NewLine +
+                "AvailablePageFile: " + AvailablePageFile + Environment.NewLine +
+                "TotalVirtual: " + TotalVirtual + Environment.NewLine +
+                "AvailableVirtual: " + AvailableVirtual + Environment.NewLine +
+                "AvailableExtendedVirtual: " + AvailableExtendedVirtual + Environment.NewLine;
+        }
+    }
+}

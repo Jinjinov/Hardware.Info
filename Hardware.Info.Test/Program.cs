38919e6fd9d0fab181962fbbaa5f109049660f4c
Daniel Isenmann <daniel.isenmann@qognify.com>
2021-04-23 15:43:30 +0200

    ServiceInfo now supports Linux

--- Hardware.Info.Test/Program.cs
+++ Hardware.Info.Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Service.Info;
+using System;
 using System.Net.NetworkInformation;
 
 namespace Hardware.Info.Test
@@ -6,6 +7,7 @@ namespace Hardware.Info.Test
     class Program
     {
         static readonly HardwareInfo hardwareInfo = new HardwareInfo();
+        static readonly ServiceInfo serviceInfo = new ServiceInfo();
 
         static void Main(string[] _)
         {
@@ -25,7 +27,11 @@ namespace Hardware.Info.Test
             //hardwareInfo.RefreshVideoControllerList();
 
             hardwareInfo.RefreshAll();
+            serviceInfo.RefreshAll();
 
+            foreach (var service in serviceInfo.Services)
+                Console.WriteLine(service);
+            
             Console.WriteLine(hardwareInfo.MemoryStatus);
 
             foreach (var hardware in hardwareInfo.BatteryList)
--- Service.Info/Linux/ServiceInfo.cs
+++ Service.Info/Linux/ServiceInfo.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 // https://www.binarytides.com/linux-commands-hardware-info/
 
@@ -8,12 +10,76 @@ namespace Service.Info.Linux
     {
         public List<Service> GetServiceList()
         {
-            return new List<Service>();
+            var serviceList = new List<Service>();
+
+            var processOutput = ReadProcessOutput("service", "--status-all");
+            var lines = processOutput.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines)
+            {
+                var service = new Service();
+                if (line.Contains("[ + ]"))
+                {
+                    service.State = ServiceState.Running;
+                }
+                else if (line.Contains("[ - ]"))
+                {
+                    service.State = ServiceState.Stopped;
+                }
+                else
+                {
+                    service.State = ServiceState.Unknown;
+                }
+                    
+                var split = line.Split(" ");
+
+                service.Name = split.Last();
+
+                var systemctlOutput = ReadProcessOutput("systemctl", $"status {service.Name}");
+                var systemctlOutputLines = systemctlOutput.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                var pid = systemctlOutputLines.FirstOrDefault(l => l.Contains("PID:"))?.Split(":").Last().Trim().Split(" ").First();
+                if (!string.IsNullOrWhiteSpace(pid))
+                {
+                    var psOutput = ReadProcessOutput("ps", $"-p {pid} -o %cpu,vsz,rss");
+                    var psOutputLines = psOutput.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+                    // Output of above ps command
+                    //%CPU   VSZ   RSS
+                    // 0.0  1463  3242 
+
+                    if (psOutputLines.Length == 2)
+                    {
+                        var values = psOutputLines.Last().Trim().Split(" ").Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+                        
+                        service.CpuUsage = Convert.ToUInt64(Math.Round(Convert.ToDecimal(values[0].Trim()), MidpointRounding.AwayFromZero));
+                        service.MemoryPrivateBytes = Convert.ToUInt64(values[1].Trim());
+                        service.MemoryWorkingSet = Convert.ToUInt64(values[2].Trim());
+                    }
+                }
+
+                serviceList.Add(service);
+            }
+            
+            return serviceList;
         }
 
         public void SetServiceAction(string serviceName, ServiceAction action)
         {
-            
+            var serviceAction = string.Empty;
+            switch (action)
+            {
+                case ServiceAction.Stop:
+                    serviceAction = "stop";
+                    break;
+                case ServiceAction.Start:
+                    serviceAction = "start";
+                    break;
+                case ServiceAction.Restart:
+                    serviceAction = "restart";
+                    break;
+            }
+
+            StartProcess("service", $"{serviceName} {serviceAction}");
         }
     }
 }

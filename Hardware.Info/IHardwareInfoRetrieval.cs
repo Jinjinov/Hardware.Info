7db4d97d2d9e71330876feb93cb6975a96a3afa3
ilCosmico <ilCosmico@gmail.com>
2025-05-28 13:40:30 +0200

    Add includePerformanceCounter flag to GetCpuList() method
    This PR introduces a new optional parameter, includePerformanceCounter, to the existing method GetCpuList(). When set to false, the method will skip the legacy PerformanceCounter call (thus avoiding any dependency on msvcr80.dll).

--- Hardware.Info/IHardwareInfoRetrieval.cs
+++ Hardware.Info/IHardwareInfoRetrieval.cs
@@ -10,7 +10,7 @@ namespace Hardware.Info
         List<Battery> GetBatteryList();
         List<BIOS> GetBiosList();
         List<ComputerSystem> GetComputerSystemList();
-        List<CPU> GetCpuList(bool includePercentProcessorTime = true, int millisecondsDelayBetweenTwoMeasurements = 500);
+        List<CPU> GetCpuList(bool includePercentProcessorTime = true, int millisecondsDelayBetweenTwoMeasurements = 500, bool includePerformanceCounter = false);
         List<Drive> GetDriveList();
         List<Keyboard> GetKeyboardList();
         List<Memory> GetMemoryList();

edcbac403ad6c099a37b32ae7fe39c39942c38ed
Urban <dzindzinovic@yahoo.com>
2026-01-02 20:21:51 +0100

    Version 101.1.1.0

---
+++ Hardware.Info.Aot.Benchmark/Program.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Hardware.Info.Benchmark
+{
+    public class Benchmarks
+    {
+        readonly IHardwareInfo _hardwareInfo = new HardwareInfo();
+
+        [Benchmark]
+        public void RefreshOperatingSystem() => _hardwareInfo.RefreshOperatingSystem();
+        [Benchmark]
+        public void RefreshMemoryStatus() => _hardwareInfo.RefreshMemoryStatus();
+        [Benchmark]
+        public void RefreshBatteryList() => _hardwareInfo.RefreshBatteryList();
+        [Benchmark]
+        public void RefreshBIOSList() => _hardwareInfo.RefreshBIOSList();
+        [Benchmark]
+        public void RefreshComputerSystemList() => _hardwareInfo.RefreshComputerSystemList();
+        [Benchmark]
+        public void RefreshCPUList() => _hardwareInfo.RefreshCPUList();
+        [Benchmark]
+        public void RefreshDriveList() => _hardwareInfo.RefreshDriveList();
+        [Benchmark]
+        public void RefreshKeyboardList() => _hardwareInfo.RefreshKeyboardList();
+        [Benchmark]
+        public void RefreshMemoryList() => _hardwareInfo.RefreshMemoryList();
+        [Benchmark]
+        public void RefreshMonitorList() => _hardwareInfo.RefreshMonitorList();
+        [Benchmark]
+        public void RefreshMotherboardList() => _hardwareInfo.RefreshMotherboardList();
+        [Benchmark]
+        public void RefreshMouseList() => _hardwareInfo.RefreshMouseList();
+        [Benchmark]
+        public void RefreshNetworkAdapterList() => _hardwareInfo.RefreshNetworkAdapterList();
+        [Benchmark]
+        public void RefreshPrinterList() => _hardwareInfo.RefreshPrinterList();
+        [Benchmark]
+        public void RefreshSoundDeviceList() => _hardwareInfo.RefreshSoundDeviceList();
+        [Benchmark]
+        public void RefreshVideoControllerList() => _hardwareInfo.RefreshVideoControllerList();
+    }
+
+    class Program
+    {
+        static void Main(string[] _)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
